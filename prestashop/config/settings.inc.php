b71ad796639a7e29c8e206f0c652a05fbedc73ca
Sofiane Sadi <sofiane.sadi@gmail.com>
2012-02-15 01:39:22 +0100

    Install prestashop application and add database dump 2
    Signed-off-by: Sofiane Sadi <sofiane.sadi@gmail.com>

---
+++ prestashop/config/settings.inc.php
@@ -0,0 +1,22 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_TYPE_', 'MySQL');
+define('_DB_NAME_', 'megamorphose');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('__PS_BASE_URI__', '/');
+define('_PS_CACHING_SYSTEM_', 'MCached');
+define('_PS_CACHE_ENABLED_', '0');
+define('_MEDIA_SERVER_1_', '');
+define('_MEDIA_SERVER_2_', '');
+define('_MEDIA_SERVER_3_', '');
+define('_THEME_NAME_', 'prestashop');
+define('_COOKIE_KEY_', 'zOa41JaQWY763Q7TjuNG0mhIXAbqYnFwLYGE6jp3qfTgF1LTz7sFIU2c');
+define('_RIJNDAEL_KEY_', 'MfWQwE5UfytKQFxRajC1QnLDJUfsPuYP');
+define('_COOKIE_IV_', 'N3nH7yRG');
+define('_RIJNDAEL_IV_', 'HTFhQjG9GEWbUHlx5o/OLg==');
+define('_PS_CREATION_DATE_', '2012-02-15');
+define('_PS_VERSION_', '1.4.6.2');
+?>
