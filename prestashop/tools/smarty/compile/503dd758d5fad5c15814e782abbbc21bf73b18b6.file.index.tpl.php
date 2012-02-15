b5ed58fe46271ae9557a1d44396052e02e8bd3a3
Sofiane Sadi <sofiane.sadi@gmail.com>
2012-02-15 01:56:07 +0100

    Mise a jour DB
    Signed-off-by: Sofiane Sadi <sofiane.sadi@gmail.com>

---
+++ prestashop/tools/smarty/compile/503dd758d5fad5c15814e782abbbc21bf73b18b6.file.index.tpl.php
@@ -0,0 +1,22 @@
+<?php /* Smarty version Smarty-3.0.7, created on 2012-02-15 01:44:16
+         compiled from "C:\Program Files (x86)\EasyPHP-5.3.9\www\Megamorphose\prestashop/themes/prestashop/index.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:326444f3affe0a41d74-92263608%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '503dd758d5fad5c15814e782abbbc21bf73b18b6' => 
+    array (
+      0 => 'C:\\Program Files (x86)\\EasyPHP-5.3.9\\www\\Megamorphose\\prestashop/themes/prestashop/index.tpl',
+      1 => 1329264672,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '326444f3affe0a41d74-92263608',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+
+
+<?php echo $_smarty_tpl->getVariable('HOOK_HOME')->value;?>
---
+++ prestashop/tools/smarty/compile/71b8b37bb6847fbf05c1facb9528f6da3070180a.file.footer.tpl.php
@@ -0,0 +1,38 @@
+<?php /* Smarty version Smarty-3.0.7, created on 2012-02-15 01:44:16
+         compiled from "C:\Program Files (x86)\EasyPHP-5.3.9\www\Megamorphose\prestashop/themes/prestashop/footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:131654f3affe0a76a58-05276053%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '71b8b37bb6847fbf05c1facb9528f6da3070180a' => 
+    array (
+      0 => 'C:\\Program Files (x86)\\EasyPHP-5.3.9\\www\\Megamorphose\\prestashop/themes/prestashop/footer.tpl',
+      1 => 1329264672,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '131654f3affe0a76a58-05276053',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+
+
+		<?php if (!$_smarty_tpl->getVariable('content_only')->value){?>
+				</div>
+
+<!-- Right -->
+				<div id="right_column" class="column">
+					<?php echo $_smarty_tpl->getVariable('HOOK_RIGHT_COLUMN')->value;?>
+
+				</div>
+			</div>
+
+<!-- Footer -->
+			<div id="footer"><?php echo $_smarty_tpl->getVariable('HOOK_FOOTER')->value;?>
+</div>
+		</div>
+	<?php }?>
+	</body>
+</html>
